802f52c26fba75822a0dfbc74f9d0d44dde5137b
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-02-14 20:56:31 +0000

    initial commit

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const userRoutes = require("./routes/user");
+const messageRoutes = require("./routes/message");
+
+const app = express();
+
+const db = require("./models/database");
+db.sequelize
+	.sync()
+	.then(() => {
+		console.log(`Synced`);
+	})
+	.catch((err) => {
+		console.log(`Failed to sync ${err.message}`);
+	});
+
+var corsOptions = {
+	origin: "http://localhost:8081",
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoutes);
+
+module.exports = app;
