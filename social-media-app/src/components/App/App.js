433931d0161febfa9bc1634563f0ed5b872fd42e
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-02-25 22:50:51 +0000

    login set token

--- social-media-app/src/components/App/App.js
+++ social-media-app/src/components/App/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import {useState} from "react"
 import{BrowserRouter, Route, Routes} from 'react-router-dom';
+
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
+import Login from '../Login/Login'
+import './App.css';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token){
+    return <Login setToken={setToken}/>
+  }
   return (
     <div className='wrapper'>
       <h1>Application</h1>
