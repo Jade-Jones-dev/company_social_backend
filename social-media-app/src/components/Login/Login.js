86a08c30c65a18714178b26c5d010638761585e8
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-03 07:29:24 +0000

    token working at login

--- social-media-app/src/components/Login/Login.js
+++ social-media-app/src/components/Login/Login.js
@@ -1,50 +1,72 @@
-import React, {useState} from "react";
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-
-import './Login.css'
+import "./Login.css";
 
 const Login = () => {
   const navigate = useNavigate();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleSubmit =  (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    fetch('http://127.0.0.1:8080/api/auth/login', {
+    fetch("http://127.0.0.1:8080/api/auth/login", {
       method: "post",
       headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
       },
-      body: JSON.stringify({email, password})
+      body: JSON.stringify({ email, password }),
     })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error))
-     
-    setPassword("");
-    setEmail("");
-    navigate("/dashboard")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Login failed");
+        }
+      })
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+        console.log(data.token);
+        navigate("/dashboard");
+        setPassword("");
+        setEmail("");
+        setErrorMessage("");
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage("Invalid email or password");
+      });
   };
 
-	return (
-		<div className='login-wrapper'>
-			<form onSubmit={handleSubmit}>
-				<label>
-					<p>Email</p>
-					<input type='email' value={email} onChange={(event) => setEmail(event.target.value)} />
-				</label>
-				<label>
-					<p>Password</p>
-					<input type='password' value={password} onChange={(event) => setPassword(event.target.value)} />
-				</label>
+  return (
+    <div className="login-wrapper">
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Email</p>
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        {errorMessage && <p className="error">{errorMessage}</p>}
         <div>
-        <button type='submit'>Log in</button>
-        </div>			
-			</form>
-		</div>
-	);
+          <button type="submit">Log in</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
+
